e0196f00845be21c321d8867a029ae24bc387445
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2021-03-11 17:59:52 -0600

    [BEAM-9185] Modify download artifacts, add filter by is_rc_version

--- release/src/main/scripts/deploy_release_candidate_pypi.sh
+++ release/src/main/scripts/deploy_release_candidate_pypi.sh
@@ -17,10 +17,24 @@
 #
 
 # This script will deploy a Release Candidate to pypi, includes:
-# 1. Build python artifacts with rc tag
+# 1. Download python binary artifacts
 # 2. Deploy Release Candidate to pypi
 
-GIT_REPO_BASE_URL=apache/beam
+set -e
+
+function clean_up(){
+  echo "Do you want to clean local clone repo ${LOCAL_CLONE_DIR}? [y|N]"
+  read confirmation
+  if [[ $confirmation = "y" ]]; then
+    cd ~
+    rm -rf ${LOCAL_CLONE_DIR}
+    echo "Cleaned up local repo."
+  fi
+}
+
+GIT_REPO_BASE_URL=benWize/beam #apache/beam
+PYTHON_ARTIFACTS_DIR=python
+BEAM_ROOT_DIR=beam
 
 echo "================Setting Up Environment Variables==========="
 if [[ -z "${RELEASE}" ]]
@@ -28,7 +42,7 @@ then
   echo "Which release version are you working on: "
   read RELEASE
 fi
-RELEASE_BRANCH=release-${RELEASE}
+RELEASE_BRANCH="BEAM-9185" #release-${RELEASE}
 if [[ -z "${RC_NUM}" ]]
 then
   echo "Which release candidate number(e.g. 1) are you going to create: "
@@ -39,8 +53,9 @@ then
   echo "Please enter your github username(ID): "
   read USER_GITHUB_ID
 fi
-echo ${RELEASE_BRANCH}
-RELEASE_COMMIT=$(git rev-parse --verify ${RELEASE_BRANCH})
+echo "${RELEASE_BRANCH}"
+RELEASE_COMMIT=$(git rev-parse --verify "${RELEASE_BRANCH}")
+LOCAL_CLONE_DIR="beam_release_${RELEASE}rc${RC_NUM}"
 
 echo "================Checking Environment Variables=============="
 echo "working on release version: ${RELEASE}"
@@ -53,7 +68,53 @@ if [[ $confirmation != "y" ]]; then
   exit
 fi
 
-#
-#echo "================Download python sources======================"
-#echo "===============Build python artifacts with rc tag============"
-#echo "====================Upload rc to pypi========================"
+echo "=====================Clear folder=============================="
+cd ~
+if [[ -d ${LOCAL_CLONE_DIR} ]]; then
+  echo "Deleting existing local clone repo ${LOCAL_CLONE_DIR}."
+  rm -rf "${LOCAL_CLONE_DIR}"
+fi
+mkdir "${LOCAL_CLONE_DIR}"
+LOCAL_CLONE_DIR_ROOT=$(pwd)/${LOCAL_CLONE_DIR}
+
+echo "================Download python artifacts======================"
+cd -
+python ./download_github_actions_artifacts.py \
+  --github-user "${USER_GITHUB_ID}" \
+  --repo-url "${GIT_REPO_BASE_URL}" \
+  --release-branch "${RELEASE_BRANCH}" \
+  --release-commit "${RELEASE_COMMIT}" \
+  --artifacts_dir "${LOCAL_CLONE_DIR_ROOT}" \
+  #--is_rc_version
+
+cd ${LOCAL_CLONE_DIR_ROOT}
+
+echo "------Checking Hash Value for apache-beam-${RELEASE}rc${RC_NUM}.zip-----"
+sha512sum -c "apache-beam-${RELEASE}rc${RC_NUM}.zip.sha512"
+
+for artifact in *.whl; do
+  echo "----------Checking Hash Value for ${artifact} wheel-----------"
+  sha512sum -c "${artifact}.sha512"
+done
+
+echo "===================Removing sha512 files======================="
+rm $(ls | grep -i ".*.sha512$")
+
+echo "====================Upload rc to pypi========================"
+virtualenv deploy_pypi_env
+source ./deploy_pypi_env/bin/activate
+pip install twine
+
+mkdir dist && mv $(ls | grep apache) dist && cd dist
+echo "Will upload the following files to PyPI:"
+ls
+echo "Are the files listed correct? [y|N]"
+read confirmation
+if [[ $confirmation != "y" ]]; then
+  echo "Exiting without deploying artifacts to PyPI."
+  clean_up
+  exit
+fi
+echo "Uploading files to pypi"
+
+clean_up
