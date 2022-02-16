787efef5e7de9139ef72e1ba58293b9ec44150fe
marco.robles <marco.robles@wizeline.com>
2022-02-16 17:37:56 -0600

    fix unit test in order to not to inject test code into main class

--- sdks/java/io/pulsar/src/main/java/org/apache/beam/sdk/io/pulsar/PulsarMessage.java
+++ sdks/java/io/pulsar/src/main/java/org/apache/beam/sdk/io/pulsar/PulsarMessage.java
@@ -1,37 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.pulsar;
 
-import com.google.common.annotations.VisibleForTesting;
-import org.apache.pulsar.client.api.Message;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.annotations.VisibleForTesting;
 
 /**
- * Class representing a Pulsar Message record. Each PulsarMessage contains a single message basic message data
- * and Message record to access directly.
+ * Class representing a Pulsar Message record. Each PulsarMessage contains a single message basic
+ * message data and Message record to access directly.
  */
 public class PulsarMessage {
-    private String topic;
-    private Long publishTimestamp;
-    private Object messageRecord;
+  private String topic;
+  private Long publishTimestamp;
+  private Object messageRecord;
 
-    @VisibleForTesting
-    public PulsarMessage(String topic, Long publishTimestamp) {
-        this.topic = topic;
-        this.publishTimestamp = publishTimestamp;
-    }
-    public PulsarMessage(String topic, Long publishTimestamp, Object messageRecord) {
-        this.topic = topic;
-        this.publishTimestamp = publishTimestamp;
-        this.messageRecord = messageRecord;
-    }
+  @VisibleForTesting
+  public PulsarMessage(String topic, Long publishTimestamp) {
+    this.topic = topic;
+    this.publishTimestamp = publishTimestamp;
+  }
 
-    public String getTopic() {
-        return topic;
-    }
+  public PulsarMessage(String topic, Long publishTimestamp, Object messageRecord) {
+    this.topic = topic;
+    this.publishTimestamp = publishTimestamp;
+    this.messageRecord = messageRecord;
+  }
 
-    public Long getPublishTimestamp() {
-        return publishTimestamp;
-    }
+  public String getTopic() {
+    return topic;
+  }
 
-    public Object getMessageRecord() {
-        return messageRecord;
-    }
+  public Long getPublishTimestamp() {
+    return publishTimestamp;
+  }
+
+  public Object getMessageRecord() {
+    return messageRecord;
+  }
 }
